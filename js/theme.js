19e1b8ee36953d22f0432d9cea7afe20672a49b9
Eduardo Cabral <eduardosbcabral@gmail.com>
2021-04-11 06:58:44 -0300

    🚧 New article and improve code

---
+++ js/theme.js
@@ -0,0 +1,25 @@
+const currentYearElement = document.querySelector('#current-year');
+currentYearElement.textContent = new Date().getFullYear();
+
+const currentTheme = localStorage.getItem('theme');
+let dark = false;
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+      dark = true;
+    }
+}
+
+const themeButton = document.querySelector('#theme-button');
+themeButton.onclick = function() {
+  dark = !dark;
+  if(dark) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
